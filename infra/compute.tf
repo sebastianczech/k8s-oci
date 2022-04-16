12693a0e1d2fa4c5b19a57c77213759104786cd0
Sebastian Czech <sebaczech@gmail.com>
2022-04-16 10:19:21 +0200

    Split main.tf into multiple files

---
+++ infra/compute.tf
@@ -0,0 +1,23 @@
+resource "oci_core_instance" "k8s_node1" {
+    availability_domain = data.oci_identity_availability_domains.ads.availability_domains[0].name
+    compartment_id = var.compartment_id
+    shape = var.instance_shape
+    source_details {
+        source_id = var.instance_image
+        source_type = "image"
+    }
+    display_name = var.instance_node1
+    create_vnic_details {
+        assign_public_ip = true
+        subnet_id = oci_core_subnet.k8s_subnet.id
+    }
+    metadata = {
+        ssh_authorized_keys = file("~/.ssh/id_rsa.pub")
+    }
+    shape_config {
+        baseline_ocpu_utilization = "BASELINE_1_1"
+        memory_in_gbs = 6
+        ocpus = 1
+    }
+    preserve_boot_volume = false
+}
