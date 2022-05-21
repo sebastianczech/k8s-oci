3f0bad06541ef5eb6b5a98872d2a35ee1fcbc25e
Sebastian Czech <sebaczech@gmail.com>
2022-05-21 19:26:31 +0200

    Improve script to checking app

--- infra/check_app.sh
+++ infra/check_app.sh
@@ -1,1 +1,1 @@
-export LB_PUBLIC_IP=`terraform output | grep lb_public_ip | awk -F\" '{print $2}'` && curl http://$LB_PUBLIC_IP
+export LB_PUBLIC_IP=`terraform output | grep lb_public_ip | awk -F\" '{print $2}'` && for i in {1..20}; do curl http://$LB_PUBLIC_IP; sleep 0.5; done
