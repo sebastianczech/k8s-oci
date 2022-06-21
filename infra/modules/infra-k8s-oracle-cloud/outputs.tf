841b303827c8d0ffcdc23056448b4a838be128ae
Sebastian Czech <sebaczech@gmail.com>
2022-06-21 21:01:14 +0200

    Add output with ID of LB

--- infra/modules/infra-k8s-oracle-cloud/outputs.tf
+++ infra/modules/infra-k8s-oracle-cloud/outputs.tf
@@ -51,3 +51,8 @@ output "lb_public_ip" {
   description = "public IPs of LB"
   value       = oci_network_load_balancer_network_load_balancer.k8s_network_load_balancer.ip_addresses[0].ip_address
 }
+
+output "lb_id" {
+  description = "ID of LB"
+  value       = oci_network_load_balancer_network_load_balancer.k8s_network_load_balancer.id
+}
