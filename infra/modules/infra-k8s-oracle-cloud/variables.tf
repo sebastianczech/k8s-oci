6090f6afb79b70956f69d1635e7e645f3d1d2b36
Sebastian Czech <sebaczech@gmail.com>
2022-07-27 23:31:24 +0200

    Validate public IP address

--- infra/modules/infra-k8s-oracle-cloud/variables.tf
+++ infra/modules/infra-k8s-oracle-cloud/variables.tf
@@ -6,6 +6,10 @@ variable "compartment_id" {
 variable "my_public_ip" {
   description = "my public IP address"
   type        = string
+  validation {
+    condition     = can(cidrnetmask(var.my_public_ip))
+    error_message = "Public IP address must be a valid IPv4 CIDR"
+  }
 }
 
 variable "id_rsa_pub" {
